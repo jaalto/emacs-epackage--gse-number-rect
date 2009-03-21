e9475f3eba8e2997a26db9833e0e264f3eb07a08
Jari Aalto <jari.aalto@cante.net>
2009-03-21 20:16:37 +0200

    gse-number-rect.el: major changes. Simplify code: use FORMAT

--- gse-number-rect.el
+++ gse-number-rect.el
@@ -2,7 +2,7 @@
 ;;   Summary:     Inserts incremental numbers in a rectangle.  I love this.
 ;;   Author:      Scott Evans <gse@antisleep.com>
 ;;   Home:        http://www.antisleep.com/elisp
-;;   Time-stamp:  <2009-03-21 19:37:33 Jari Aalto (jaalto)>
+;;   Time-stamp:  <2009-03-21 20:14:13 Jari Aalto (jaalto)>
 ;;
 ;; Commentary:
 ;;   Pretty self-expanatory.  If you have text like:
@@ -44,67 +44,41 @@
 ;; 2002.04.05 gse Add documentation.
 ;;---------------------------------------------------------------------------
 
-(require 'rect)
-
-;;---------------------------------------------------------------------------
-
-(defvar gse-number-rectangle-min-width nil
-  "*If non-nil, an integer that dictates the minimum width
-of numbered rectangles.  This controls minimum zero-padding;
-i.e. if it is 2, less than 10 lines will still be numbered
-01, 02, 03, etc.  If it is 3, 001, 002, 003.")
-
-;;---------------------------------------------------------------------------
+(autoload 'apply-on-rectangle "rect")
 
 (defvar gse-number-rectangle-count nil)
 (defvar gse-number-rectangle-history nil)
 
-;;---------------------------------------------------------------------------
-
-(defun gse-number-rectangle-callback (start end format-string suffix-text)
+(defun gse-number-rectangle-callback (start end format-string)
   (move-to-column start t)
   (setq gse-number-rectangle-count (+ gse-number-rectangle-count 1))
-  (insert (format format-string gse-number-rectangle-count) suffix-text))
+  (insert (format format-string gse-number-rectangle-count)))
 
-;;---------------------------------------------------------------------------
-
-(defun gse-number-rectangle (start end start-at suffix-text &optional prefix)
-  "Insert rising numbers on each line of the region-rectangle, shifting text right.
-The left edge of the rectangle specifies the column for insertion.  Numbers are
-zero-padded by default.
+(defun gse-number-rectangle (beg end start-at format &optional no-zero-padding)
+  "Insert numbers in front of lines in rectangle BEG END.
 
 START-AT specifices the first number to start at.
-SUFFIX-TEXT is inserted after the numbers.
+FORMAT is format string where %i denotes number.
+If NO-ZERO-PADDING is non-nil, do not padd numbers with leading zeroes.
 
-Variable gse-number-rectangle-min-width controls minimum zero-padding;
-i.e. if it is 2, less than 10 lines will still be numbered 01, 02, 03, etc.
-If it is 3, 001, 002, 003.
-
-Optional prefix means do not zero-pad numbers, regardless of
-gse-number-rectangle-min-width."
+See variable `gse-number-rectangle-min-width' for zero padding control."
   (interactive
    (list
-    (region-beginning) ; start
-    (region-end) ; end
+    (region-beginning)
+    (region-end)
     (if (functionp 'read-number)
-        (read-number "First number [1]: " 1) ; start-at
-      (string-to-int (read-string "First number [1]: " nil nil "1"))) ; start-at
-    (read-string "Suffix: " " " 'gse-number-rectangle-history) ; suffix-text
-    current-prefix-arg)) ; prefix
-
+        (read-number "First number [1]: " 1)
+      (string-to-int (read-string "First number [1]: " nil nil "1")))
+    (read-string "Format: " "%i " 'gse-number-rectangle-history)
+    current-prefix-arg))
   (setq start-at (- start-at 1))
-
-  (let ((format-string "%i"))
-    (if (not prefix)
-        (let* ((max (+ (count-lines start end) start-at))
-               (longest (length (int-to-string (+ 1 max)))))
-          (if (and (integerp gse-number-rectangle-min-width)
-                   (< longest gse-number-rectangle-min-width))
-              (setq longest gse-number-rectangle-min-width))
-          (setq format-string (concat "%0" (int-to-string longest) "i"))))
-
-    (setq gse-number-rectangle-count start-at)
-    (apply-on-rectangle 'gse-number-rectangle-callback start end format-string suffix-text)))
+  (unless no-zero-padding
+    (let* ((max     (+ (count-lines beg end) start-at))
+	   (longest (length (int-to-string (+ 1 max))))
+	   (fmt     (concat "%0" (int-to-string longest) "i")))
+      (setq format (replace-regexp-in-string "%i" fmt format))))
+  (setq gse-number-rectangle-count start-at)
+  (apply-on-rectangle 'gse-number-rectangle-callback beg end format))
 
 ;;---------------------------------------------------------------------------
 
