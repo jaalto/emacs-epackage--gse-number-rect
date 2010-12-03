acb7f03566396060ec0abd2be708c6f85b593f73
Jari Aalto <jari.aalto@cante.net>
2010-12-03 12:59:59 +0200

    epackage/: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ epackage/gse-number-rect-autoloads.el
@@ -0,0 +1,3 @@
+(autoload 'gse-number-rectangle-callback        "gse-number-rect" "" nil)
+(autoload 'gse-number-rectangle                 "gse-number-rect" "" t)
+(provide 'gse-number-rect-autoloads)
---
+++ epackage/gse-number-rect-install.el
@@ -0,0 +1,6 @@
+(autoload 'gse-number-rectangle                 "gse-number-rect" "" t)
+
+(unless (lookup-key global-map "\C-xrN")
+  (global-set-key "\C-xrN" 'gse-number-rectangle))
+
+(provide 'gse-number-rect-xactivate)
